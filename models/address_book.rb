975340c44ed14eb636a547a7ecd54c8997806e23
Bradley Gohman <bradley.s.gohman@gmail.com>
2015-09-18 12:24:36 -0500

    reading from a csv file

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,4 +1,6 @@
 require_relative "entry.rb"
+require "csv"
+
 class AddressBook
   attr_accessor :entries
 
@@ -22,6 +24,16 @@ class AddressBook
       if entry.name == name && entry.phone_number == phone_number && entry.email == email
         @entries.delete(entry)
       end
+    end
+  end
+
+  def import_from_csv(file_name)
+    csv_text = File.read(file_name)
+    csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
+
+    csv.each do |row|
+      row_hash = row.to_hash
+      add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
     end
   end
 
